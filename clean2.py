6bfa95779560e0642694a811d2c66bd64549dac9
ochiailee64 <ochiailee64@gmail.com>
2017-08-01 12:40:47 -0700

    Add files via upload

---
+++ clean2.py
@@ -0,0 +1,18 @@
+
+
+a = [30, 34, 75, 27, 8, 58, 10, 1, 59, 2322]
+def dropsort(list):
+    print list
+    if sorted(list) == list:
+        print "list is sorted"
+        return None
+    c = []
+    if list[0] < list[1]:
+        c.append(list[0])
+    else:
+       c.append(list[1])
+    for x in range(0,len(list) - 1 ):
+        if list[x] < list[x+1]:
+            c.append(list[x+1])
+    dropsort(c)
+dropsort(a)
